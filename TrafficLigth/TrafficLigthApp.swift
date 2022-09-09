6d18fbff4ba8d55b9a9c020df7290e6a2e199059
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2022-09-09 16:34:07 +0300

    Initial Commit

---
+++ TrafficLigth/TrafficLigthApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLigthApp.swift
+//  TrafficLigth
+//
+//  Created by Lera Savchenko on 9.09.22.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLigthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
